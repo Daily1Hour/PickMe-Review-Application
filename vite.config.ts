e4ab8f13fa9bcbab764134e7cefea874ae484903
NarciSource <jwch11@gmail.com>
2024-11-24 14:47:09 +0900

    fix: MFA 모드 지원을 위한 설정 변경

--- vite.config.ts
+++ vite.config.ts
@@ -9,26 +9,33 @@ import vitePluginSingleSpa from "vite-plugin-single-spa";
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    const serverPort = Number(process.env.VITE_SERVER_PORT);
-    const spaEntryPoints = "src/app/spa.tsx";
-    const isSPA = mode === "spa";
+    const serverPort = Number(process.env.VITE_SERVER_PORT); // 서버포트
+    const spaEntryPoints = "src/app/mfa.tsx";
+    const isMFA = mode === "mfa";
 
     return {
         plugins: [
-            tsconfigPaths(),
-            nodePolyfills(),
-            vitePluginSingleSpa({
-                serverPort,
-                spaEntryPoints,
-            }),
+            tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
+            nodePolyfills(), // node.js 라이브러리 polyfills를 적용
+            isMFA &&
+                vitePluginSingleSpa({
+                    // single-spa 라이브러리 적용
+                    serverPort,
+                    spaEntryPoints,
+                }),
             createHtmlPlugin({
+                // EJS 템플릿 엔진을 사용하여 HTML 파일 생성
                 minify: true,
                 inject: {
                     data: {
-                        isSPA,
+                        isMFA,
                     },
                 },
             }),
         ],
+        server: {
+            port: serverPort,
+            cors: true,
+        },
     };
 });
