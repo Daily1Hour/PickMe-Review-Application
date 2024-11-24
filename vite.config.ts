9f80a5c6bd70fe832643f4630a60a4fbc868c6c3
NarciSource <jwch11@gmail.com>
2024-11-24 13:14:43 +0900

    chore: vite 환경변수 업데이트

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig, loadEnv } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return {};
+});
