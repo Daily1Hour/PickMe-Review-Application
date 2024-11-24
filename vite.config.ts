bc7365e4cad24712d3eb0b8561291aa34121f947
NarciSource <jwch11@gmail.com>
2024-11-24 13:19:27 +0900

    chore: vite-plugin-node-polyfills 추가
    - 브라우저 환경에서 Node의 핵심 모듈을 폴리필하기 위한 Vite 플러그인

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig, loadEnv } from "vite";
 
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
     return {
-        plugins: [tsconfigPaths()],
+        plugins: [tsconfigPaths(), nodePolyfills()],
     };
 });
