2373ce5e968a4596b9a23c958891c1b1720128d6
NarciSource <jwch11@gmail.com>
2024-11-30 20:35:59 +0900

    feat: 마이크로프론트엔드 지원을 위한 설정 및 파일 추가

--- src/app/application.tsx
+++ src/app/application.tsx
@@ -15,6 +15,4 @@ const lifecycle = singleSpaReact({
     },
 });
 
-export const bootstrap = lifecycle.bootstrap;
-export const mount = lifecycle.mount;
-export const unmount = lifecycle.unmount;
+export const { bootstrap, mount, unmount } = lifecycle;
--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,52 @@ import { defineConfig, loadEnv } from "vite";
 
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import vitePluginSingleSpa from "vite-plugin-single-spa";
+import vitePluginSingleSpa, { SingleSpaPluginOptions } from "vite-plugin-single-spa";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
+    const isProduction = null; // = mode === "production";
     const serverPort = Number(process.env.VITE_SERVER_PORT); // 서버포트
-    const spaEntryPoints = "src/app/mfa.tsx";
-    const isMFA = mode === "mfa";
+
+    let vitePluginSingleSpaOptions: SingleSpaPluginOptions | undefined;
+
+    switch (process.env.VITE_MF_TYPE) {
+        case "application":
+            vitePluginSingleSpaOptions = {
+                serverPort,
+                spaEntryPoints: "src/app/application.tsx",
+            };
+            break;
+        case "parcel":
+            vitePluginSingleSpaOptions = {
+                serverPort,
+                spaEntryPoints: "src/app/parcel.tsx",
+            };
+            break;
+    }
 
     return {
         plugins: [
             tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
             nodePolyfills(), // node.js 라이브러리 polyfills를 적용
-            isMFA &&
-                vitePluginSingleSpa({
-                    // single-spa 라이브러리 적용
-                    serverPort,
-                    spaEntryPoints,
-                }),
+            isProduction && vitePluginSingleSpa(vitePluginSingleSpaOptions), // single-spa 라이브러리 적용
         ],
+        build: {
+            rollupOptions: {
+                input: {
+                    index: "index.html", // 일반 빌드용 진입점
+                    application: "./src/app/application.tsx",
+                    parcel: "./src/app/parcel.tsx",
+                },
+            },
+        },
         server: {
+            // 개발 서버 설정
             port: serverPort,
             cors: true,
         },
+        base: process.env.VITE_PUBLIC_URL,
     };
 });
