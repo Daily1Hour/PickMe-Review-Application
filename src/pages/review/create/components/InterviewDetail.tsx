a5634387dc7b14b220bde1ef9f9b9e80d10f703b
wlstn <wlstn159159@naver.com>
2024-12-30 17:20:10 +0900

    feat: 면접 기본 정보 입력창 생성

--- src/pages/review/create/components/InterviewDetail.tsx
+++ src/pages/review/create/components/InterviewDetail.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import { Input, Stack, Fieldset } from "@chakra-ui/react";
+import { Field } from "@/widgets/chakra-ui/field";
 
 const InterviewDetail = () => {
-    return <div>InterviewDetail</div>;
+    return (
+        <Fieldset.Root size="lg" maxW="100%">
+            <Stack>
+                <Fieldset.Legend>면접 정보</Fieldset.Legend>
+                <Fieldset.HelperText></Fieldset.HelperText>
+            </Stack>
+
+            <Fieldset.Content>
+                <Field
+                    orientation="horizontal"
+                    label="회사명"
+                    paddingBottom="10px"
+                >
+                    <Input
+                        placeholder="회사명"
+                        variant="flushed"
+                        name="companyName"
+                        size="lg"
+                    />
+                </Field>
+
+                <Field
+                    orientation="horizontal"
+                    label="지원 직무"
+                    paddingBottom="10px"
+                >
+                    <Input
+                        name="position"
+                        variant="flushed"
+                        placeholder="지원 직무"
+                        size="lg"
+                    />
+                </Field>
+
+                <Field
+                    orientation="horizontal"
+                    label="면접 날짜"
+                    paddingBottom="10px"
+                >
+                    <Input
+                        name="interviewDateTime"
+                        type="datetime-local"
+                        variant="flushed"
+                        placeholder="면접 날짜"
+                        size="lg"
+                    />
+                </Field>
+
+                <Field
+                    orientation="horizontal"
+                    label="면접 유형"
+                    paddingBottom="10px"
+                >
+                    <Input
+                        name="category"
+                        variant="flushed"
+                        placeholder="면접 유형"
+                        size="lg"
+                    />
+                </Field>
+            </Fieldset.Content>
+        </Fieldset.Root>
+    );
 };
 
 export default InterviewDetail;
--- src/pages/review/create/index.tsx
+++ src/pages/review/create/index.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import { Box, Heading, Button } from "@chakra-ui/react";
+
 import InterviewDetail from "./components/InterviewDetail";
 
 const CreateReviewPage = () => {
-    return <InterviewDetail />;
+    return (
+        <Box
+            flex="1"
+            bg="whiteAlpha.100"
+            paddingLeft="30%" // 좌측 패딩 10%
+            paddingRight="30%" // 우측 패딩 10%
+            display="flex"
+            flexDirection="column"
+            height="100vh" // 화면 전체 높이를 채움
+            alignItems="left" // 수직 정렬
+            justifyContent="flex-start" // 수평 정렬
+        >
+            <Heading textAlign="center" size="3xl" marginBottom="30px">
+                면접 회고 작성
+            </Heading>
+
+            <InterviewDetail />
+
+            <Button colorPalette="green">저장</Button>
+        </Box>
+    );
 };
 
 export default CreateReviewPage;
