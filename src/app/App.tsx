7888a91a7c63af7ecb6068746f7f29a5b1f163f4
wlstn <wlstn159159@naver.com>
2024-12-30 16:02:28 +0900

    refactor: <BrowserRouter>로 교체하고 ReviewPage와 CreateReviewPage에 대한 라우트 정의

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Provider as ChakraProvider } from "@/widgets/chakra-ui/provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReviewPage from "@/pages/review";
+import CreateReviewPage from "@/pages/review/ui/CreateReviewPage";
 
 export default function App(): React.ReactElement {
     return (
         <ChakraProvider>
-            <ReviewPage />
+            <BrowserRouter>
+                <Routes>
+                    <Route index path="/" element={<ReviewPage />}></Route>
+                    <Route
+                        path="/create-review"
+                        element={<CreateReviewPage />}
+                    ></Route>
+                </Routes>
+            </BrowserRouter>
         </ChakraProvider>
     );
 }
