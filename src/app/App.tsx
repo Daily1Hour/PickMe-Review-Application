1860bda4db14af4b02ed0228b4d98ed4d14727a0
wlstn <wlstn159159@naver.com>
2024-12-30 16:14:53 +0900

    refactor: CreateReviewPage를 create 폴더에 index파일로 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider as ChakraProvider } from "@/widgets/chakra-ui/provider";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReviewPage from "@/pages/review";
-import CreateReviewPage from "@/pages/review/ui/CreateReviewPage";
+import CreateReviewPage from "@/pages/review/create";
 
 export default function App(): React.ReactElement {
     return (
