ebd700bbcd49d4613963e524a1611073909206b2
wlstn <wlstn159159@naver.com>
2025-01-24 18:37:13 +0900

    style: 불필요한 코드 삭제

--- src/features/review/ui/ReviewForm.tsx
+++ src/features/review/ui/ReviewForm.tsx
@@ -1,10 +1,7 @@
-import { useNavigate } from "react-router-dom";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { FormProvider, useForm } from "react-hook-form";
 import { Heading, Box } from "@chakra-ui/react";
 
 import { initialFormData } from "../api/initialFormData";
-import { putReviewApi, postReviewApi, deleteReviewApi } from "../api";
 import { InterviewReviewParts } from "../ui";
 import { InterviewReviews } from "@/entities/review/model/review";
 import {
