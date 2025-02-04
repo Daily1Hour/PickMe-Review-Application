5aca2390729b46a636e2c6e2a2f360b67f58ce13
wlstn <wlstn159159@naver.com>
2025-02-04 16:41:36 +0900

    refactor: SideDrawer 사용

--- src/features/side/ui/side.tsx
+++ src/features/side/ui/side.tsx
@@ -1,26 +1,11 @@
-import { useState } from "react";
-import { Flex } from "@chakra-ui/react";
 import { getSideData } from "../api/sideApi";
 import { useQuery } from "@tanstack/react-query";
 import { GetSideDTO } from "../api/getSideDTO";
-import { useNavigate } from "react-router-dom";
-import ButtonItem from "./ButtonItem";
-import ReviewList from "./ReviewList";
-import SearchBar from "./SearchBar";
-import SidebarHeader from "./SidebarHeader";
-import SidebarToggleButton from "./SidebarToggleButton";
-import SidebarContainer from "./SidebarContainer";
-import { useReviewIdStore } from "@/shared/store/useReviewIdStore";
 import { useSideStore } from "../store/useSideStore";
+import SideDrawer from "./SideDrawer";
 
 const Sidebar = () => {
-    const [isSidebarVisible, setSidebarVisible] = useState(true);
-
     const { searchQuery } = useSideStore();
-
-    const { setReviewId } = useReviewIdStore();
-
-    const navigate = useNavigate();
 
     const { data } = useQuery<GetSideDTO[]>({
         queryKey: ["side"],
@@ -36,29 +21,7 @@ const Sidebar = () => {
         item.label.toLowerCase().includes(searchQuery.toLowerCase()),
     );
 
-    return (
-        <Flex height="100vh">
-            <SidebarToggleButton
-                onClick={() => setSidebarVisible(!isSidebarVisible)}
-            />
-
-            {isSidebarVisible && (
-                <SidebarContainer>
-                    <SidebarHeader title={"목록"} />
-                    <ButtonItem
-                        label={"새 작성"}
-                        onClick={() => {
-                            navigate("/");
-                            setReviewId(undefined);
-                        }}
-                        justifyContent={"center"}
-                    />
-                    <SearchBar />
-                    <ReviewList filteredItems={filteredItems} />
-                </SidebarContainer>
-            )}
-        </Flex>
-    );
+    return <SideDrawer filteredItems={filteredItems} />;
 };
 
 export default Sidebar;
