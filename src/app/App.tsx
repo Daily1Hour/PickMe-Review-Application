af04b3383d5c09073904795000ecc2ef315b2cce
NarciSource <jwch11@gmail.com>
2025-02-16 12:35:20 +0900

    feat: chakraUiSystem 스타일 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Provider as ChakraProvider } from "@/shared/chakra-ui/provider";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ChakraProvider } from "@chakra-ui/react";
+import { chakraUiSystem } from "@styleguide/react";
+
 import router from "./router";
 
 const queryClient = new QueryClient();
 
 export default function App(): React.ReactElement {
     return (
-        <QueryClientProvider client={queryClient}>
-            <ChakraProvider>
+        <ChakraProvider value={chakraUiSystem}>
+            <QueryClientProvider client={queryClient}>
                 <RouterProvider router={router} />
-            </ChakraProvider>
-        </QueryClientProvider>
+            </QueryClientProvider>
+        </ChakraProvider>
     );
 }
