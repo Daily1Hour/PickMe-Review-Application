ad9c6c8449d190608ce533009bcee6907e4e5015
NarciSource <jwch11@gmail.com>
2025-02-16 12:28:47 +0900

    chore: module federation 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,43 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
     // 환경변수 증설
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    // 서버포트
-    const serverPort = Number(process.env.VITE_SERVER_PORT);
+    const serverPort = Number(process.env.VITE_SERVER_PORT); // 서버 포트
+    const styleguideUrl = process.env.VITE_STYLEGUIDE_URL!; // 스타일가이드 URL
 
     // vite 설정
     return {
         plugins: [
             react(), // React 라이브러리 적용
+            federation({
+                // 모듈 페더레이션 적용
+                remotes: {
+                    "@styleguide": styleguideUrl,
+                },
+                shared: {
+                    react: {
+                        requiredVersion: "^18.3.1",
+                    },
+                    "react-dom": {
+                        requiredVersion: "^18.3.1",
+                    },
+                    "@chakra-ui/react": {
+                        requiredVersion: "^3.2.3",
+                    },
+                }, // 공유 모듈 중복 번들링 방지
+            }),
             tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
             nodePolyfills(), // node.js 라이브러리 polyfills를 적용
         ],
         build: {
             emptyOutDir: false,
+            target: "esnext",
         },
         server: {
             // 개발 서버 설정
--- vite.single-spa.config.ts
+++ vite.single-spa.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import federation from "@originjs/vite-plugin-federation";
 import vitePluginSingleSpa, {
     SingleSpaPluginOptions,
 } from "vite-plugin-single-spa";
@@ -11,8 +12,8 @@ export default defineConfig(({ mode }) => {
     // 환경변수 증설
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-    // 서버포트
-    const serverPort = Number(process.env.VITE_SERVER_PORT);
+    const serverPort = Number(process.env.VITE_SERVER_PORT); // 서버 포트
+    const styleguideUrl = process.env.VITE_STYLEGUIDE_URL!; // 스타일가이드 URL
 
     // single-spa 옵션 설정
     const vitePluginSingleSpaOptions: SingleSpaPluginOptions = {
@@ -35,14 +36,32 @@ export default defineConfig(({ mode }) => {
     return {
         plugins: [
             react(), // React 라이브러리 적용
+            federation({
+                // 모듈 페더레이션 적용
+                remotes: {
+                    "@styleguide": styleguideUrl,
+                },
+                shared: {
+                    react: {
+                        requiredVersion: "^18.3.1",
+                    },
+                    "react-dom": {
+                        requiredVersion: "^18.3.1",
+                    },
+                    "@chakra-ui/react": {
+                        requiredVersion: "^3.2.3",
+                    },
+                }, // 공유 모듈 중복 번들링 방지
+            }),
             tsconfigPaths(), // tsconfig.json의 paths 설정을 적용
             vitePluginSingleSpa(vitePluginSingleSpaOptions), // single-spa 라이브러리 적용
         ],
         build: {
-            // cssMinify: false, // css 코드 압축 여부
-             minify: false, // 빌드시 코드 압축 여부
+            cssMinify: true, // css 코드 압축 여부
+            minify: true, // 빌드시 코드 압축 여부
             cssCodeSplit: false, // css 코드 분할 여부
             emptyOutDir: false, // 빌드시 기존 파일 삭제 여부
+            target: "esnext", // 빌드 대상
         },
     };
 });
