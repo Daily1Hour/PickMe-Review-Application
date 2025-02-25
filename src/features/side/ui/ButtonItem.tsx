502161150a0d4d95a6d01b45513d67f4092414a5
wlstn <wlstn159159@naver.com>
2025-02-25 16:58:55 +0900

    refactor: useNavigate 대신 NavLink 사용

--- src/features/side/ui/ButtonItem.tsx
+++ src/features/side/ui/ButtonItem.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@chakra-ui/react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 interface ButtonItemProps {
     itemId: string;
@@ -12,24 +13,19 @@ const ButtonItem = ({
     label,
     justifyContent = "start",
 }: ButtonItemProps) => {
-    const navigate = useNavigate();
-
     const { reviewId } = useParams<{ reviewId: string | undefined }>();
 
-    // 클릭된 아이템의 ID가 현재 reviewId와 같으면 navigate를 호출하지 않음
-    const handleButtonClick = (itemId: string) => {
-        navigate(`${itemId}`); // 클릭 시 item.id 값을 URL에 추가하여 해당 경로로 이동
-    };
     return (
-        <Button
-            variant={reviewId === itemId ? "solid" : "ghost"}
-            colorScheme={reviewId === itemId ? "teal" : "gray"}
-            mb="10px"
-            onClick={() => handleButtonClick(itemId)}
-            justifyContent={justifyContent} // 텍스트를 왼쪽 정렬
-        >
-            {label}
-        </Button>
+        <NavLink to={`${itemId}`}>
+            <Button
+                variant={reviewId === itemId ? "solid" : "ghost"}
+                colorScheme={reviewId === itemId ? "teal" : "gray"}
+                mb="10px"
+                justifyContent={justifyContent} // 텍스트를 왼쪽 정렬
+            >
+                {label}
+            </Button>
+        </NavLink>
     );
 };
 
