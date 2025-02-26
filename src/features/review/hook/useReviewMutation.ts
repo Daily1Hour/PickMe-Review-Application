4c5d56388ccbbe37425609ece1105fb748d0c029
wlstn <wlstn159159@naver.com>
2025-02-26 15:45:49 +0900

    refactor: 생성, 수정 mutate 분리

--- src/features/review/hook/useReviewMutation.ts
+++ src/features/review/hook/useReviewMutation.ts
@@ -1,42 +1,43 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { useNavigate } from "react-router-dom";
 
 import { putReviewApi, postReviewApi, deleteReviewApi } from "../api";
 import { FlattenedReview } from "@/entities/review/model/review";
 
 export const useReviewMutation = () => {
-    const navigate = useNavigate();
     const queryClient = useQueryClient();
 
-    const mutation = useMutation({
+    const { mutateAsync: createMutation } = useMutation({
+        mutationFn: ({ data }: { data: FlattenedReview }) =>
+            postReviewApi(data),
+        onSuccess: () => {
+            // // 생성 성공 시 사이드 바 "side" 쿼리의 캐시를 무효화하고 데이터를 새로 가져옴(refetch)
+            queryClient.refetchQueries({
+                queryKey: ["side"],
+            });
+        },
+    });
+
+    const { mutate: updateMutation } = useMutation({
         mutationFn: async ({
             reviewId,
             data,
         }: {
-            reviewId: string | undefined;
+            reviewId: string;
             data: FlattenedReview;
-        }) => {
-            if (reviewId) {
-                return await putReviewApi(data, reviewId);
-            } else {
-                return await postReviewApi(data);
-            }
-        },
-        onSuccess: (data) => {
-            // // 생성 & 수정 성공 시 사이드 바 "side" 쿼리의 캐시를 무효화하고 데이터를 새로 가져옴(refetch)
+        }) => putReviewApi(data, reviewId),
+        onSuccess: (reviewId) => {
+            // 수정 성공 시 쿼리의 캐시를 무효화하고 데이터를 새로 가져옴(refetch)
             queryClient.refetchQueries({
                 queryKey: ["side"],
             });
 
             queryClient.refetchQueries({
-                queryKey: ["review", `${data.data.interviewDetailId}`],
+                queryKey: ["review", reviewId],
             });
-
-            navigate(`${data.data.interviewDetailId}`);
         },
     });
 
-    const deleteMutation = useMutation({
+    const { mutate: deleteMutation } = useMutation({
         mutationFn: async (reviewId: string | undefined) => {
             if (reviewId) return deleteReviewApi(reviewId);
         },
@@ -44,9 +45,7 @@ export const useReviewMutation = () => {
             queryClient.refetchQueries({
                 queryKey: ["side"],
             });
-            // 삭제 시 초기 화면으로
-            navigate("/");
         },
     });
-    return { mutation, deleteMutation };
+    return { createMutation, updateMutation, deleteMutation };
 };
