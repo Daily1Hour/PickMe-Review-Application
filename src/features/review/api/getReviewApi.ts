274b544f7b6ba15b011e11697896b99150740657
wlstn <wlstn159159@naver.com>
2025-04-04 16:48:32 +0900

    refactor: 토큰 전달 방식 변경

--- src/features/review/api/getReviewApi.ts
+++ src/features/review/api/getReviewApi.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
+import { accessToken } from "@/shared/api/tokens";
 import { dtoToReview } from "../service/dtoToReview";
 import { GetResponseDTO } from "./reviewDTOList";
 
 const SERVER_URL = import.meta.env.VITE_SERVER_URL;
-const TOKEN = import.meta.env.VITE_TOKEN;
 
 const client = axios.create({
     baseURL: `${SERVER_URL}/review`,
     headers: {
-        Authorization: `Bearer ${TOKEN}`,
+        Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json",
     },
 });
