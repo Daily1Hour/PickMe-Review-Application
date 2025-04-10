4b72e45f270baf45d0ac8a39018978055a76fe1d
wlstn <wlstn159159@naver.com>
2025-04-10 15:17:00 +0900

    refactor: 면접 회고 화면 스켈레톤 컴포넌트를 회고 폼 화면 형태로 리팩토링

--- src/features/review/ui/ReviewSkeleton.tsx
+++ src/features/review/ui/ReviewSkeleton.tsx
@@ -1,9 +1,16 @@
-import { Skeleton } from "@chakra-ui/react";
-
-import React from "react";
+import { Flex, Skeleton } from "@chakra-ui/react";
 
 const ReviewSkeleton = () => {
-    return <Skeleton height="200px" />;
+    return (
+        <Flex direction="column" height="500px" gap="50px" marginTop="50px">
+            <Flex alignSelf="center">
+                <Skeleton height="50px" width="300px" />
+            </Flex>
+            <Skeleton height="200px" />
+            <Skeleton height="200px" />
+            <Skeleton height="200px" />
+        </Flex>
+    );
 };
 
 export default ReviewSkeleton;
