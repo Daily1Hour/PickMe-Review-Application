363189783490e071e4cf4766582e53f4687b16e8
wlstn <wlstn159159@naver.com>
2025-04-10 14:54:14 +0900

    feat: 면접 회고 작성 화면 스켈레톤 UI 컴포넌트 생성

---
+++ src/features/review/ui/ReviewSkeleton.tsx
@@ -0,0 +1,9 @@
+import { Skeleton } from "@chakra-ui/react";
+
+import React from "react";
+
+const ReviewSkeleton = () => {
+    return <Skeleton height="200px" />;
+};
+
+export default ReviewSkeleton;
