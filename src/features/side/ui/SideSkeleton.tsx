a24ab5ebc29929a7cf99aec74e08db3dbb4fcc59
wlstn <wlstn159159@naver.com>
2025-04-10 15:44:37 +0900

    refactor: 스켈레톤 가로 정렬 -> 세로 정렬

--- src/features/side/ui/SideSkeleton.tsx
+++ src/features/side/ui/SideSkeleton.tsx
@@ -1,12 +1,16 @@
-import { Skeleton } from "@chakra-ui/react";
+import { Skeleton, VStack } from "@chakra-ui/react";
 import { Item } from "@styleguide/react";
 
 const SideSkeleton = () => {
-    return Array.from({ length: 4 }).map((_, idx) => (
-        <Item key={idx}>
-            <Skeleton height="50px" width="100%" borderRadius="md" />
-        </Item>
-    ));
+    return (
+        <VStack width="100%">
+            {Array.from({ length: 4 }).map((_, idx) => (
+                <Item key={idx}>
+                    <Skeleton height="50px" width="100%" borderRadius="md" />
+                </Item>
+            ))}
+        </VStack>
+    );
 };
 
 export default SideSkeleton;
