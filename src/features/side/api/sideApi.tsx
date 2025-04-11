0fdc0b35cef350a39d673ce4bd1b6bbd7d39d946
wlstn <wlstn159159@naver.com>
2025-04-11 14:52:51 +0900

    refactor: try-catch로 api 요청 예외처리

--- src/features/side/api/sideApi.tsx
+++ src/features/side/api/sideApi.tsx
@@ -1,6 +1,12 @@
 import { client } from "@/shared/api";
 
 export const getSideData = async () => {
-    const response = await client.get("/interview/side");
-    return response.data;
+    try {
+        const response = await client.get("/interview/side");
+        return response.data;
+    } catch (err: any) {
+        console.error("백엔드 에러 메시지:", err.response?.data?.message);
+        alert(err.response?.data?.message);
+        throw err; //즉, 이 함수(getSideData)를 호출한 쪽에서 에러를 또 처리할 수 있게, 예를 들어 상위 컴포넌트나 useQuery에서 onError로 처리 가능하게!
+    }
 };
