51a3ba372aab00e87c45edc2ef5b006ab8ef3f7e
wlstn <wlstn159159@naver.com>
2025-04-11 14:58:46 +0900

    refactor: alert는 삭제

--- src/features/side/api/sideApi.tsx
+++ src/features/side/api/sideApi.tsx
@@ -6,7 +6,6 @@ export const getSideData = async () => {
         return response.data;
     } catch (err: any) {
         console.error("백엔드 에러 메시지:", err.response?.data?.message);
-        alert(err.response?.data?.message);
         throw err; //즉, 이 함수(getSideData)를 호출한 쪽에서 에러를 또 처리할 수 있게, 예를 들어 상위 컴포넌트나 useQuery에서 onError로 처리 가능하게!
     }
 };
