998a6cc94d26292fb7c2a82f3678937626bcf1a8
wlstn <wlstn159159@naver.com>
2025-04-11 16:49:22 +0900

    refactor: 코드 수정 및 삭제

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -7,12 +7,9 @@ const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 export default axios.create({
     baseURL: SERVER_URL,
     headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${idToken}`,
         "X-Access-Token": `Bearer ${accessToken}`,
         Accept: "application/json",
         "Content-Type": "application/json",
-    },
-    validateStatus: (status) => {
-        return status >= 200 && status < 400; // 400 아래는 따로 처리
     },
 });
